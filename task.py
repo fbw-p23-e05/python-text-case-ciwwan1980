33a18266317fbce8105ea45822c79c0d5ad22ba1
ciwwan1980 <ajilqado@gmail.com>
2023-08-31 14:42:33 +0200

    task-upper-lower-case

---
+++ task.py
@@ -0,0 +1,8 @@
+
+original_string = input("Original String: ")
+
+string_in_uppercase = original_string.upper()
+string_in_lowercase = original_string.lower()
+
+print("String in uppercase:", string_in_uppercase)
+print("String in lowercase:", string_in_lowercase)
