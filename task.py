056c5d0bc270e4dd403b4c1f6785e80b6dcdb581
ciwwan1980 <ajilqado@gmail.com>
2023-09-02 20:03:32 +0200

    task

--- task.py
+++ task.py
@@ -1,8 +1,21 @@
 
-original_string = input("Original String: ")
+# original_string = input("Original String: ")
 
-string_in_uppercase = original_string.upper()
-string_in_lowercase = original_string.lower()
+# string_in_uppercase = original_string.upper()
+# string_in_lowercase = original_string.lower()
 
-print("String in uppercase:", string_in_uppercase)
-print("String in lowercase:", string_in_lowercase)
+# print("String in uppercase:", string_in_uppercase)
+# print("String in lowercase:", string_in_lowercase)
+    
+
+
+original_string = "The Quick BroWnFoX jumps over the laZy Dog!"
+
+
+uppercase_string = original_string.upper()
+
+lowercase_string = original_string.lower()
+
+print("Original String:", original_string)
+print("String in uppercase:", uppercase_string)
+print("String in lowercase:", lowercase_string)
